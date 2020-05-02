258115e4fcc5f92a08016a4f19519aaf22653966
svmihar <svmihar@gmail.com>
2020-05-02 23:19:35 +0700

    completed challenge

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,80 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: FlatButton(
+                    color: Colors.red,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play(
+                          'note1.wav'); // secara default bakal nyari di assets/
+                    },
+                    child: Text('')),
+              ),
+              Container(
+                child: FlatButton(
+                    color: Colors.blue,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play(
+                          'note2.wav'); // secara default bakal nyari di assets/
+                    },
+                    child: Text('')),
+              ),
+              Container(
+                child: FlatButton(
+                    color: Colors.yellow,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play(
+                          'note3.wav'); // secara default bakal nyari di assets/
+                    },
+                    child: Text('')),
+              ),
+              Container(
+                child: FlatButton(
+                    color: Colors.orange,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play(
+                          'note4.wav'); // secara default bakal nyari di assets/
+                    },
+                    child: Text('')),
+              ),
+              Container(
+                child: FlatButton(
+                    color: Colors.teal,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play(
+                          'note5.wav'); // secara default bakal nyari di assets/
+                    },
+                    child: Text('')),
+              ),
+              Container(
+                child: FlatButton(
+                    color: Colors.pink,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play(
+                          'note6.wav'); // secara default bakal nyari di assets/
+                    },
+                    child: Text('')),
+              ),
+              Container(
+                child: FlatButton(
+                    color: Colors.indigo,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play(
+                          'note7.wav'); // secara default bakal nyari di assets/
+                    },
+                    child: Text('')),
+              ),
+            ],
+          ),
         ),
       ),
     );
